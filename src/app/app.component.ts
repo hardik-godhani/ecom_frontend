48b5a3e49d890139846641fe9de563cd4c4d569e
hardik-godhani <hardikgodhani6@gmail.com>
2023-03-09 06:52:23 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ecom-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ecom';
+}
